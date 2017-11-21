midchildan/lkl-linux

lkl: add support of raw-ipencap virtio-net backend
This is particularly useful when we use rmnet0 on andriod-6, which is an
interface for cellar device (i.e., 3G or LTE).

Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

tools/lkl/lib/hijack/init.c
@@ -328,7 +328,11 @@ hijack_init(void)
 					nd[i] = lkl_netdev_vde_create(ifparams[i]);
 				*/
 				else if (strcmp(iftype[i], "raw") == 0)
-					nd[i] = lkl_netdev_raw_create(ifparams[i]);
+					nd[i] = lkl_netdev_raw_create(
+						ifparams[i], 1);
+				else if (strcmp(iftype[i], "raw-ipenc") == 0)
+					nd[i] = lkl_netdev_raw_create(
+						ifparams[i], 0);
 			}
 		}
 
@@ -409,6 +413,12 @@ hijack_init(void)
 			else
 				fprintf(stderr, "failed to get ifindex for netdev id %d: %s\n",
 						nd_id[i], lkl_strerror(nd_ifindex));
+
+			int lkl_netdev_ipencap_conf(int ifindex, struct lkl_netdev *nd);
+			if (nd[i]->is_ip_encap)
+				lkl_netdev_ipencap_conf(nd_ifindex, nd[i]);
+
+
 		}
 
 		if (nd_ifindex >= 0 && mtu_str[i]) {

cosminstoica/mptcp

Cherry-picked from: 5cd05d020ca98151
Conflicts:
	tools/lkl/lib/hijack/init.c
	tools/lkl/lib/virtio_net_raw.c

tools/lkl/lib/hijack/init.c
@@ -286,7 +286,9 @@ static int lkl_hijack_netdev_create(struct lkl_config *cfg, int ifidx)
 		if (strcmp(cfg->iftype[ifidx], "vde") == 0)
 			nd[ifidx] = lkl_netdev_vde_create(cfg->ifparams[ifidx]);
 		if (strcmp(cfg->iftype[ifidx], "raw") == 0)
-			nd[ifidx] = lkl_netdev_raw_create(cfg->ifparams[ifidx]);
+			nd[ifidx] = lkl_netdev_raw_create(cfg->ifparams[ifidx], 1);
+		if (strcmp(cfg->iftype[ifidx], "raw-ipenc") == 0)
+			nd[ifidx] = lkl_netdev_raw_create(cfg->ifparams[ifidx], 0);
 	}
 
 	if (nd[ifidx]) {
@@ -332,6 +334,12 @@ static int lkl_hijack_netdev_configure(struct lkl_config *cfg, int ifidx)
 				"failed to get ifindex for netdev id %d: %s\n",
 				nd_id[ifidx], lkl_strerror(nd_ifindex));
 	}
+
+	/* */
+	int lkl_netdev_ipencap_conf(int ifindex, struct lkl_netdev *nd);
+	if (nd[ifidx]->is_ip_encap)
+		lkl_netdev_ipencap_conf(nd_ifindex, nd[ifidx]);
+
 
 	if (nd_ifindex >= 0 && cfg->ifmtu_str[ifidx]) {
 		int mtu = atoi(cfg->ifmtu_str[ifidx]);